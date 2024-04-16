6be1bf0446d5c2ba7003ae53138133380ec2aaeb
Ahmed REFAI-ext <ahmed.refai-ext@sanofi.com>
2024-04-16 11:06:25 +0200

    feat: tweak reward function

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -3,7 +3,7 @@ import math
 MAX_ANGLE = 90
 TOTAL_NUM_STEPS = 120
 MAX_SPEED = 4
-MIN_SPEED = 1.1
+MIN_SPEED = 1
 FORCAST = 6
 
 
@@ -44,7 +44,7 @@ class Reward:
         print('speed_diff: ', speed_diff)
         reward_speed = speed_diff / (MAX_SPEED - MIN_SPEED)
         print('reward_speed formula: speed_diff / (MAX_SPEED - MIN_SPEED): ', reward_speed)
-        c = 2
+        c = 1
         print(f'formula for total reward: -{c}x2-{c}y2+{c}')
         return -c * reward_direction ** 2 - c * reward_speed ** 2 + c
 
@@ -93,8 +93,8 @@ def reward_function(params):
     steps = params['steps']
     progress = params['progress']
     if (steps % 30) == 0 and progress > (steps / TOTAL_NUM_STEPS) * 100:
-        reward += 20
-        print('reward +3.5 for efficiency')
+        reward += 3
+        print('reward +3 for efficiency')
 
     print('reward final result: ', reward)
     return float(min(1e3, max(reward, 1e-3)))
