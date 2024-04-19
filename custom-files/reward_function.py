c1bcdde053c3659a95480476b0f8343720111f9c
Ahmed REFAI-ext <ahmed.refai-ext@sanofi.com>
2024-04-19 02:03:34 +0200

    feat: tweak reward function

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -4,7 +4,7 @@ MAX_SPEED = 4
 MIN_SPEED = 1
 MAX_STEERING = 30
 MIN_STEERING = -30
-FORCAST = 20
+FORCAST = 25
 STEPS_PER_SECOND = 15
 VEHICLE_WIDTH = 0.225
 
@@ -52,16 +52,22 @@ class Reward:
             prev_heading = self.prev_heading + self.prev_steering_angle
         else:
             prev_heading = heading
-        if abs(prev_heading-heading) > 20:
+        self.prev_steering_angle = params['steering_angle']
+        self.prev_heading = params['heading']
+        diff_heading = abs(prev_heading - heading)
+        if diff_heading > 180:
+            diff_heading = 360 - diff_heading
+        print('diff_heading in degrees:', diff_heading)
+        if diff_heading > 20:
             return 1e-3
+        w = 5 - (0.25 * diff_heading)
+        print('weight:', w)
         direction_diff = abs(track_direction - heading)
         if direction_diff > 180:
             direction_diff = 360 - direction_diff
         print('direction_diff in degrees:', direction_diff)
         if direction_diff > 40:
             return 1e-3
-        reward_direction = 40 * (1 - (direction_diff / 40))
-        print('reward_direction:', reward_direction)
 
         # speed
         p_w = waypoints[closest_waypoints[0]]
@@ -74,21 +80,19 @@ class Reward:
         speed = params['speed']
         speed_diff = abs(target_speed - speed)
         print('speed_diff: ', speed_diff)
-        reward_speed = (MAX_SPEED - MIN_SPEED) * (1 - (speed_diff / (MAX_SPEED - MIN_SPEED)))
-        print('reward_speed:', reward_speed)
+
 
         # steering
-        prev_steering_angle = self.prev_steering_angle if self.prev_steering_angle is not None else 0
-        steering_angle = params['steering_angle']
-        steering_diff = abs(steering_angle - prev_steering_angle)
-        print('steering_diff in degrees:', steering_diff)
-        if steering_diff > 30:
-            return 1e-3
-        reward_steering = 10 * (1 - (steering_diff / 30))
-        print('reward_steering:', reward_steering)
-        self.prev_steering_angle = steering_angle
-
-        return reward_direction * reward_speed + reward_steering
+        # prev_steering_angle = self.prev_steering_angle if self.prev_steering_angle is not None else 0
+        # steering_angle = params['steering_angle']
+        # steering_diff = abs(steering_angle - prev_steering_angle)
+        # print('steering_diff in degrees:', steering_diff)
+        # if steering_diff > 30:
+        #     return 1e-3
+        # reward_steering = 10 * (1 - (steering_diff / 30))
+        # print('reward_steering:', reward_steering)
+        # self.prev_steering_angle = steering_angle
+        return -w * (direction_diff / 40) ** 2 - w * (speed_diff / (MAX_SPEED - MIN_SPEED)) ** 2 + w
 
 
 reward_state = Reward()
@@ -110,8 +114,7 @@ def reward_function(params):
     steps = params['steps']
     if is_final_step:
         if is_complete_lap:
-            reward += 100 / (steps / STEPS_PER_SECOND)
-        if is_offtrack or is_reversed:
-            reward -= steps
+            time = steps / STEPS_PER_SECOND
+            reward += 1000 / time
     print('reward final result: ', reward)
     return float(min(1e3, max(reward, 1e-3)))
