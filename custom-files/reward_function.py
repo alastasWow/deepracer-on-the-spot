69f223c769346adbaff494a6f739aeacd9e1f383
Ahmed REFAI-ext <ahmed.refai-ext@sanofi.com>
2024-04-25 15:09:20 +0200

    feat: reward function v1 progress & top

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -7,6 +7,7 @@ MIN_STEERING = -30
 FORCAST = 15
 STEPS_PER_SECOND = 15
 VEHICLE_WIDTH = 0.225
+TOP_CONST = 200
 
 
 def forcast(start_forcast_index, next_waypoint, waypoints, track_width, pos):
@@ -37,93 +38,22 @@ def dist(p1, p2, p3):
 
 class Reward:
     def __init__(self):
-        self.prev_steering_angle = None
-        self.jerome = 1
+        self.last_progress = 0
+        self.nb_sortie = 0
 
     def reward_function(self, params):
-        # waypoints = params['waypoints']
-        # closest_waypoints = params['closest_waypoints']
-        # track_width = params['track_width']
-        # potential_forcast_index = (closest_waypoints[1] + FORCAST) % len(waypoints)
-        # car_pos = (params['x'], params['y'])
-        # print('============')
-        # print('car forcast')
-        # forcast_car_index, diff_index = forcast(potential_forcast_index, closest_waypoints[1], waypoints, track_width, car_pos)
-        # forcast_car = waypoints[forcast_car_index]
-        # print('============')
 
-        # print('track forcast')
-        # forcast_track_index = forcast(potential_forcast_index, closest_waypoints[1], waypoints, track_width, waypoints[closest_waypoints[0]])
-        # forcast_track = waypoints[forcast_track_index]
-
-        # steering
-        # if self.prev_steering_angle is not None:
-        #     prev_steering_angle = self.prev_steering_angle
-        # else:
-        #     prev_steering_angle = params['steering_angle']
-        # self.prev_steering_angle = params['steering_angle']
-        # diff_steering = abs(prev_steering_angle - params['steering_angle'])
-        # print('diff_steering in degrees:', diff_steering)
-        # prev_steering_angle = self.prev_steering_angle if self.prev_steering_angle is not None else 0
-        # steering_angle = params['steering_angle']
-        # steering_diff = abs(steering_angle - prev_steering_angle)
-        # print('steering_diff in degrees:', steering_diff)
-        # if steering_diff > 30:
-        #     return 1e-3
-        # reward_steering = 10 * (1 - (steering_diff / 30))
-        # print('reward_steering:', reward_steering)
-        # self.prev_steering_angle = steering_angle
-
-        # direction
-        # car_direction = math.atan2(forcast_car[1] - car_pos[1], forcast_car[0] - car_pos[0])
-        # car_direction = math.degrees(car_direction)
-        # heading = params['heading']
-        # direction_diff = abs(car_direction - heading)
-        # if direction_diff > 180:
-        #     direction_diff = 360 - direction_diff
-        # print('direction_diff:', direction_diff)
-        # print('============')
-
-        # speed
-        # p_w = waypoints[closest_waypoints[0]]
-        # n_w = waypoints[closest_waypoints[1]]
-        # curve_forcast = waypoints[(closest_waypoints[1] + 6) % len(waypoints)]
-        # track_curve = math.atan2(n_w[1] - p_w[1], n_w[0] - p_w[0]) - math.atan2(curve_forcast[1] - p_w[1], curve_forcast[0] - p_w[0])
-        # track_curve = abs(math.degrees(track_curve))
-        # if track_curve > 180:
-        #     track_curve = 360 - track_curve
-        # print('track_curve:', track_curve)
-        # print('track_curve:', track_curve)
-        # if track_curve < 45:
-        #     return params['speed']
-        # elif 45 <= track_curve < 60:
-        #     return params['speed'] * 0.5
-        # else:
-        #     return 1e-3
-        # if direction_diff <= 90:
-        # else:
-        #     target_speed = -4
-        # target_speed = (MAX_SPEED - MIN_SPEED) * math.exp(-5 * (track_curve / 90) ** 2) + MIN_SPEED
-        # target_speed = MAX_SPEED - ((MAX_SPEED - MIN_SPEED) / 90) * track_curve
-        # print('target_speed:', target_speed)
-        # speed_diff = abs(target_speed - params['speed'])
-        # print('speed_diff:', speed_diff)
-        # print('============')
-
-        # x, y = (direction_diff / 180), speed_diff / (MAX_SPEED - MIN_SPEED)
-        # reward = -(2 * x) - (2 * y) + 1
-        # reward = - 5 * x ** 2 - 5 * y ** 2 + 1.001
-        # reward = (MAX_SPEED - MIN_SPEED) - ((MAX_SPEED - MIN_SPEED) / 90) * track_curve
-        # reward = params['speed'] * math.exp(-5 * (track_curve / 90) ** 2)
-        # reward = 1e-3
         all_wheels_on_track = params['all_wheels_on_track']
         if not all_wheels_on_track:
-            reward = 1e-3
-            self.jerome += 1
+            self.nb_sortie += 1
+
+        nb_progress = params['progress'] - self.last_progress
+        self.last_progress = params['progress']
+        top = params['steps'] + 45 * self.nb_sortie
+        if top < TOP_CONST:
+            return (100 - (top / 2)) * nb_progress
         else:
-            x, y = params['speed'], params['steps']
-            reward = (math.exp(x + 2) + ((200 - y) / 10)) / self.jerome
-        return reward
+            return 1e-3
 
 
 reward_state = Reward()
