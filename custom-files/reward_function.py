fd74d61e0e73a365d422f4c12cfbcf062bd37bcf
Jerome Besnainou <jerome.besnainou@edifixio.com>
2024-04-26 23:22:42 +0200

    Model v2 - reset à fin de tour + minimastion de punition sortie de piste ... à voir

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -8,7 +8,7 @@ MIN_STEERING = -30
 FORCAST = 15
 STEPS_PER_SECOND = 15
 VEHICLE_WIDTH = 0.225
-TOP_CONST = 200
+TOP_CONST = 199
 PUNITION_SORTIE_FACTOR = 1.1
 
 
@@ -37,6 +37,109 @@ def forcast(start_forcast_index, next_waypoint, waypoints, track_width, pos):
 def dist(p1, p2, p3):
     return abs((p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])) / math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
 
+class RewardV3:
+    def __init__(self):
+        self.iteration=0
+        self.reInit()
+
+    def reInit(self):
+        #Global Iteration
+        self.iteration += 1
+        #UUID
+        self.uuid = uuid.uuid4()
+        #Progress
+        self.lastCurrentProgress = 0
+        self.lastProgress = 0
+        self.totalProgress = 0
+        #Out
+        self.outCount = 0
+        self.outLastTime = False
+        #Reward
+        self.totalReward = 0
+        #Closest Way Point
+        self.lastClosestWayPoint = 0
+        #Step
+        self.stepCount = 0
+
+    def reward_function(self, params):
+        speed = params['speed']
+        closest_waypoints = params['closest_waypoints']
+        all_wheels_on_track = params['all_wheels_on_track']
+        if (self.endLap(params)):
+            self.reInit()
+        reward = self.regularStep(params)
+        self.totalReward += reward
+        print(f'### jerome - iteration {self.iteration}, uuid {self.uuid}, speed {speed}, all_wheels_on_track {all_wheels_on_track}, lastCurrentProgress {self.lastCurrentProgress} lastProgress {self.lastProgress}, totalProgress {self.totalProgress}, outCount {self.outCount}, outLastTime {self.outLastTime}, totalReward {self.totalReward}, lastClosestWayPoint {self.lastClosestWayPoint}, closest_waypoints {closest_waypoints}')
+        return reward
+
+    def regularStep(self,params):
+        #Manage Step
+        self.manageStep(params)
+        #Check if out
+        wasOut = self.outLastTime
+        if (not self.manageOut(params)):
+            #Not out
+            #Check progression
+            currentProgress = self.manageProgression(params,wasOut)
+            self.lastCurrentProgress=currentProgress
+            #Attibruate reward
+            self.manageRewardForProgression(params,currentProgress)
+            return ((100 - (self.stepCount / 2)) * currentProgress)+1
+        else:
+            #Out
+            self.lastCurrentProgress=0
+            return 1e-3
+
+    def manageRewardForProgression(self,params,currentProgress):
+        top=self.stepCount
+        if top < TOP_CONST:
+            return 5+((100 - (self.stepCount / 2)) * currentProgress)
+        else:
+            return 1+currentProgress
+
+    def manageProgression(self,params,wasOut):
+        progress = params['progress']
+        if (progress>self.lastProgress):
+            #We made a progress
+            currentProgress = progress - self.lastProgress
+            self.totalProgress += currentProgress
+            self.lastProgress = progress
+            return currentProgress
+        else:
+            #No progress
+            if (not wasOut):
+                #No progress strange
+                return 0
+            else:
+                #No progress due to out
+                currentProgress = progress
+                self.totalProgress += currentProgress
+                self.lastProgress = progress
+                return currentProgress
+
+    def manageStep(self,params):
+        self.stepCount += 1
+
+    def manageOut(self,params):
+        all_wheels_on_track = params['all_wheels_on_track']
+        if not all_wheels_on_track:
+            #Vehicule out
+            if (self.outLastTime):
+                #We were already out
+                return True
+            else:
+                self.outLastTime = True
+                self.outCount += 1
+                return True
+        else:
+            return False
+
+    def endLap(self,params):
+        newClosestWayPoint = params['closest_waypoints'][0]
+        if (newClosestWayPoint<(self.lastClosestWayPoint+10)):
+            return True
+        else:
+            return False
 
 class Reward:
     def __init__(self):
@@ -46,6 +149,7 @@ class Reward:
         self.totalProgress = 0
         self.totalReward = 0
         self.jeromeStep = 0
+        self.lastClosest = 0
 
     def reward_function(self, params):
         self.jeromeStep += 1
@@ -53,6 +157,7 @@ class Reward:
         if not all_wheels_on_track:
             self.nb_sortie += 1
         #COucou
+        newLastClosest = params['closest_waypoints'][1]
         speed = params['speed']
         closest_waypoints = params['closest_waypoints']
         nb_progress = params['progress'] - self.last_progress
@@ -72,12 +177,11 @@ class Reward:
         print(f'### jerome - uuid {self.uuid}, jeromeStep {self.jeromeStep}, currentStep {currentStep}, top {top}, closest_waypoints {closest_waypoints}, speed {speed}, nb_progress {nb_progress}, lastProgress {self.last_progress}, totalProgress {self.totalProgress}, nbSortie {self.nb_sortie}, all_wheels_on_track {all_wheels_on_track}, reward {reward} and totalReward {self.totalReward}')
         return reward
 
-reward_state = Reward()
+reward_state = RewardV3()
 
 
 def reward_function(params):
     print('params =>', params)
-
     reward = reward_state.reward_function(params)
     print('reward final result: ', reward)
     return float(min(1e3, max(reward, 1e-3)))
