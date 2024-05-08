8260958ff2cbb2177e54038fee2fefc13285b5bc
Jerome Besnainou <jerome.besnainou@edifixio.com>
2024-05-08 15:29:11 +0200

    Model v6.0b - juste pour se remettre dans le bain avec j'espère la bonne branche de rebase

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,12 +1,19 @@
 import math
+import uuid
 
-MAX_SPEED = 3.5
+MAX_SPEED = 4
 MIN_SPEED = 1
-MAX_STEERING = 25
-MIN_STEERING = -25
-FORCAST = 10
+MAX_STEERING = 30
+MIN_STEERING = -30
+FORCAST = 15
 STEPS_PER_SECOND = 15
 VEHICLE_WIDTH = 0.225
+TOP_CONST = 140
+BONUS_PROGRESSION = 5
+BONUS_NOT_OUT_BUT_NO_PROGRESS = 1
+PUNITION_SORTIE_FACTOR = 1.1
+ELIGIBILITY_BONUS_END_PROGRESSION = 99
+BONUS_END_PROGRESSION_FACTOR = 60
 
 
 def forcast(start_forcast_index, next_waypoint, waypoints, track_width, pos):
@@ -19,150 +26,240 @@ def forcast(start_forcast_index, next_waypoint, waypoints, track_width, pos):
         out_of_track = False
         for j in range(i + 1, len(waypoints_middle)):
             d = dist(waypoints[waypoints_middle[i]], pos, waypoints[waypoints_middle[j]])
-            if d >= 0.5 * track_width:
+            if d >= 0.5 * (track_width + VEHICLE_WIDTH):
                 out_of_track = True
                 break
         if not out_of_track:
             forcast_index = waypoints_middle[i]
             break
-    # print('projected waypoint:', forcast_index)
+    print('projected waypoint:', forcast_index)
     diff_index = forcast_index - next_waypoint if forcast_index > next_waypoint else forcast_index + len(waypoints) - 1 - next_waypoint
-    # print('diff_index:', diff_index)
+    print('diff_index:', diff_index)
     return forcast_index, diff_index
 
 
 def dist(p1, p2, p3):
     return abs((p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])) / math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
 
-
-class Reward:
-    prev_progress = 0
-    total_reward = 0
-
+class RewardV3:
     def __init__(self):
-        self.reset()
+        self.iteration=0
+        self.reInit()
 
-    def reset(self):
-        self.prev_progress = 0
-        self.total_reward = 0
+    def reInit(self):
+        #Global Iteration
+        self.iteration += 1
+        #UUID
+        self.uuid = uuid.uuid4()
+        #Progress
+        self.lastCurrentProgress = 0
+        self.lastProgress = 0
+        self.maxLastProgress = 0
+        self.totalProgress = 0
+        self.consumedEndProgressionBonus = 0
+        #Out
+        self.outCount = 0
+        self.outLastTime = False
+        #Reward
+        self.totalReward = 0
+        #Closest Way Point
+        self.lastClosestWayPoint = 0
+        #Step
+        self.stepCount = 0
 
     def reward_function(self, params):
-        """
-        # waypoints = params['waypoints']
-        # closest_waypoints = params['closest_waypoints']
-        # track_width = params['track_width']
-        # potential_forcast_index = (closest_waypoints[1] + FORCAST) % len(waypoints)
-        # car_pos = (params['x'], params['y'])
-        # print('============')
-        # print('car forcast')
-        # forcast_car_index, diff_index = forcast(potential_forcast_index, closest_waypoints[1], waypoints, track_width, car_pos)
-        # forcast_car = waypoints[potential_forcast_index]
-
-        # print('============')
-        # print('track forcast')
-        # forcast_track_index = forcast(potential_forcast_index, closest_waypoints[1], waypoints, track_width, waypoints[closest_waypoints[0]])
-        # forcast_track = waypoints[forcast_track_index]
-
-        # steering
-        # if self.prev_steering_angle is not None:
-        #     prev_steering_angle = self.prev_steering_angle
-        # else:
-        #     prev_steering_angle = params['steering_angle']
-        # self.prev_steering_angle = params['steering_angle']
-        # diff_steering = abs(prev_steering_angle - params['steering_angle'])
-        # print('diff_steering in degrees:', diff_steering)
-        # prev_steering_angle = self.prev_steering_angle if self.prev_steering_angle is not None else 0
-        # steering_angle = params['steering_angle']
-        # steering_diff = abs(steering_angle - prev_steering_angle)
-        # print('steering_diff in degrees:', steering_diff)
-        # if steering_diff > 30:
-        #     return 1e-3
-        # reward_steering = 10 * (1 - (steering_diff / 30))
-        # print('reward_steering:', reward_steering)
-        # self.prev_steering_angle = steering_angle
-
-        # direction
-        # car_direction = math.atan2(forcast_car[1] - car_pos[1], forcast_car[0] - car_pos[0])
-        # car_direction = math.degrees(car_direction)
-        # heading = params['heading']
-        # direction_diff = abs(car_direction - heading)
-        # if direction_diff > 180:
-        #     direction_diff = 360 - direction_diff
-        # print('direction_diff:', direction_diff)
-        # print('============')
-
-        # speed
-        # p_w = waypoints[closest_waypoints[0]]
-        # n_w = waypoints[closest_waypoints[1]]
-        # curve_forcast = waypoints[(closest_waypoints[1] + 3) % len(waypoints)]
-        # track_curve = math.atan2(n_w[1] - p_w[1], n_w[0] - p_w[0]) - math.atan2(curve_forcast[1] - p_w[1], curve_forcast[0] - p_w[0])
-        # track_curve = abs(math.degrees(track_curve))
-        # if track_curve > 180:
-        #     track_curve = 360 - track_curve
-        # print('track_curve:', track_curve)
-        # print('track_curve:', track_curve)
-        # if track_curve < 45:
-        #     return params['speed']
-        # elif 45 <= track_curve < 60:
-        #     return params['speed'] * 0.5
-        # else:
-        #     return 1e-3
-        # if direction_diff <= 90:
-        # else:
-        #     target_speed = -4
-        # target_speed = (MAX_SPEED - MIN_SPEED) * math.exp(-5 * (track_curve / 90) ** 2) + MIN_SPEED
-        # target_speed = MAX_SPEED - ((MAX_SPEED - MIN_SPEED) / 90) * track_curve
-        # print('target_speed:', target_speed)
-        # speed_diff = abs(target_speed - params['speed'])
-        # print('speed_diff:', speed_diff)
-        # print('============')
-        # reward = -(2 * x) - (2 * y) + 1
-        # reward = - 5 * x ** 2 - 5 * y ** 2 + 1.001
-        # reward = (MAX_SPEED - MIN_SPEED) - ((MAX_SPEED - MIN_SPEED) / 90) * track_curve
-
-        # progress_diff = params['progress'] - self.progress
-        # self.progress = params['progress']
-        # reward = (progress_diff / self.steps)
-        """
-        reward = 1e-3
-        all_wheels_on_track = params['all_wheels_on_track']
-        track_width = params['track_width']
-        distance_from_center = params['distance_from_center']
-        progress = params['progress']
         speed = params['speed']
-        waypoints = params['waypoints']
         closest_waypoints = params['closest_waypoints']
-        MAX_DIST = 0.5 * (track_width + VEHICLE_WIDTH)
-        if all_wheels_on_track and distance_from_center < MAX_DIST:
-            potential_forcast_index = (closest_waypoints[1] + FORCAST) % len(waypoints)
-            car_pos = (params['x'], params['y'])
-            forcast_car_index, diff_index = forcast(potential_forcast_index, closest_waypoints[1], waypoints, track_width, car_pos)
-            # w1 = round(1 - (diff_index / FORCAST) ** 3, 2)
-            w1 = math.exp(-50*(diff_index / FORCAST) ** 15)
-            w2 = 1 - w1
-            reward = round(100 * w1, 2) * (progress - self.prev_progress) + round(100 * w2, 2) * (speed / MAX_SPEED)
-            print(f'{closest_waypoints}, {diff_index}')
-            print(f'{reward} = {round(100 * w1, 2)} * {progress - self.prev_progress} + {round(100 * w2, 2)} * {speed / MAX_SPEED}')
-        self.prev_progress = progress
-        self.total_reward += reward
-        is_complete_lap = int(progress) == 100
+        all_wheels_on_track = params['all_wheels_on_track']
+        distance_from_center = params['distance_from_center']
+        newClosestWayPoint = params['closest_waypoints'][0]
+        self.lastClosestWayPoint = newClosestWayPoint
+        reward = self.regularStep(params)
+        self.totalReward += reward
+        print(f'### jerome - iteration {self.iteration}, uuid {self.uuid}, reward {reward}, speed {speed}, all_wheels_on_track {all_wheels_on_track}, distance_from_center {distance_from_center}, stepCount {self.stepCount}, lastCurrentProgress {self.lastCurrentProgress}, lastProgress {self.lastProgress}, maxLastProgress {self.maxLastProgress}, totalProgress {self.totalProgress}, consumedEndProgressionBonus {self.consumedEndProgressionBonus}, outCount {self.outCount}, outLastTime {self.outLastTime}, totalReward {self.totalReward}, lastClosestWayPoint {self.lastClosestWayPoint}, closest_waypoints {closest_waypoints}')
+        if (self.endLap(params)):
+            self.reInit()
+        return reward
+
+    def regularStep(self,params):
+        #Manage Step
+        self.manageStep(params)
+        #Check if out
+        wasOut = self.outLastTime
+        if (not self.manageOut(params)):
+            #Not out
+            #Check progression
+            currentProgress = self.manageProgression(params,wasOut)
+            self.lastCurrentProgress=currentProgress
+            #Attibruate reward
+            return self.manageRewardForProgression(params,currentProgress)
+        else:
+            #Out
+            self.lastCurrentProgress=0
+            return 1e-3
+
+    def manageRewardForProgression(self,params,currentProgress):
+        if (currentProgress>0):
+            distance_from_center = params['distance_from_center']
+            if (distance_from_center>0.6):
+                #100% ok 12% échec à 0.8
+                return 1e-2
+            if (distance_from_center<0.2807):
+                #100% ok 0% échec
+                #0.8 bonusCenter = 22
+                #0.4 bonusCenter = 35
+                bonusCenter = 33
+            else:
+                #Here we are between 0.2807 and 0.8
+                #bonusCenter = math.exp(6*(0.8-distance_from_center))-0.99
+                #from 34.84 to 0.1
+                #bonusCenter = math.exp(30*(0.4-distance_from_center))-0.9
+                #from 34.84 to 0.1
+                bonusCenter = math.exp(11*(0.6-distance_from_center))-0.9
+            top = self.stepCount + (PUNITION_SORTIE_FACTOR * 45 * self.outCount)
+            if top < TOP_CONST:
+                return self.bonusEndProgression(params,top)+((bonusCenter + BONUS_PROGRESSION + (100 - (top/2))) * currentProgress)
+                #return bonusCenter + BONUS_PROGRESSION + ((100 - (top / 2)) * currentProgress)
+            else:
+                return (bonusCenter + BONUS_NOT_OUT_BUT_NO_PROGRESS) * currentProgress
+                #return bonusCenter + BONUS_NOT_OUT_BUT_NO_PROGRESS + currentProgress
+        else:
+            #No progress
+            return 1e-3
+
+    def bonusEndProgression(self,params,top):
+        progress = params['progress']
+        if (progress>ELIGIBILITY_BONUS_END_PROGRESSION):
+            bonusFactor = (progress-ELIGIBILITY_BONUS_END_PROGRESSION)*BONUS_END_PROGRESSION_FACTOR
+            basedBonusEndProgression = ((bonusFactor*bonusFactor/2)-self.consumedEndProgressionBonus)
+            self.consumedEndProgressionBonus+=basedBonusEndProgression
+            bonusEndProgression = (basedBonusEndProgression*TOP_CONST/top)
+            print(f'### marcelBonusEndProgression - iteration {self.iteration}, uuid {self.uuid}, progress {progress}, bonusEndProgression {bonusEndProgression}, top {top} => consumedEndProgressionBonus {self.consumedEndProgressionBonus}')
+        else:
+            #No Bonus for end of progression
+            return 0
+
+    def manageProgression(self,params,wasOut):
+        progress = params['progress']
+        currentProgress = progress - self.maxLastProgress
+        self.totalProgress = progress
+        self.lastProgress = progress
+        if (currentProgress>0):
+            #We made a progress
+            self.maxLastProgress = progress
+            return currentProgress
+        else:
+            #No progresse do not change maxLastProgress
+            if (not wasOut):
+                #No progress strange : we keep previous totalProgress and lastProgress
+                print(f'### marcelWasOut - iteration {self.iteration}, uuid {self.uuid}, progress {progress}, stepCount {self.stepCount}, lastCurrentProgress {self.lastCurrentProgress}, lastProgress {self.lastProgress}, maxLastProgress {self.maxLastProgress}, totalProgress {self.totalProgress}, consumedEndProgressionBonus {self.consumedEndProgressionBonus}, outCount {self.outCount}, outLastTime {self.outLastTime}, totalReward {self.totalReward}, lastClosestWayPoint {self.lastClosestWayPoint}')
+                return 0
+            else:
+                #No progress due to out
+                return 0
+
+    def manageProgressionOld(self,params,wasOut):
+        progress = params['progress']
+        if (progress>self.lastProgress):
+            #We made a progress
+            currentProgress = progress - self.lastProgress
+            self.totalProgress += currentProgress
+            self.lastProgress = progress
+            return currentProgress
+        else:
+            #No progress
+            if (not wasOut):
+                #No progress strange : we keep previous totalProgress and lastProgress
+                print(f'### marcelWasOut - iteration {self.iteration}, uuid {self.uuid}, progress {progress}, stepCount {self.stepCount}, lastCurrentProgress {self.lastCurrentProgress}, lastProgress {self.lastProgress}, maxLastProgress {self.maxLastProgress}, totalProgress {self.totalProgress}, consumedEndProgressionBonus {self.consumedEndProgressionBonus}, outCount {self.outCount}, outLastTime {self.outLastTime}, totalReward {self.totalReward}, lastClosestWayPoint {self.lastClosestWayPoint}')
+                return 0
+            else:
+                #No progress due to out
+                currentProgress = progress
+                self.totalProgress += currentProgress
+                self.lastProgress = progress
+                return currentProgress
+
+    def manageStep(self,params):
+        self.stepCount += 1
+
+    def manageOut(self,params):
+        all_wheels_on_track = params['all_wheels_on_track']
+        if not all_wheels_on_track:
+            #Vehicule out
+            if (self.outLastTime):
+                #We were already out
+                return True
+            else:
+                self.outLastTime = True
+                self.outCount += 1
+                return True
+        else:
+            self.outLastTime = False
+            return False
+
+    def endLap(self,params):
+        progress = params['progress']
         is_offtrack = params['is_offtrack']
-        is_reversed = params['is_reversed']
-        is_crashed = params['is_crashed']
-        is_final_step = is_complete_lap or is_offtrack or is_reversed or is_crashed
-        if is_final_step:
-            # if is_complete_lap:
-            #     reward += 0.1 * self.total_reward
-            #     print(f'completed lap with original total reward {self.total_reward} and new reward {reward}')
-            self.reset()
-        return float(min(1e3, max(reward, 1e-3)))
+        if (is_offtrack):
+            print(f'### marcelOffTrack - iteration {self.iteration}, uuid {self.uuid}, progress {progress}, is_offtrack {is_offtrack}, stepCount {self.stepCount}, lastCurrentProgress {self.lastCurrentProgress}, lastProgress {self.lastProgress}, maxLastProgress {self.maxLastProgress}, totalProgress {self.totalProgress}, consumedEndProgressionBonus {self.consumedEndProgressionBonus}, outCount {self.outCount}, outLastTime {self.outLastTime}, totalReward {self.totalReward}, lastClosestWayPoint {self.lastClosestWayPoint}')
+            return True
+        if (progress==100):
+            print(f'### marcel100Progression - iteration {self.iteration}, uuid {self.uuid}, progress {progress}, is_offtrack {is_offtrack}, stepCount {self.stepCount}, lastCurrentProgress {self.lastCurrentProgress}, lastProgress {self.lastProgress}, maxLastProgress {self.maxLastProgress}, totalProgress {self.totalProgress}, consumedEndProgressionBonus {self.consumedEndProgressionBonus}, outCount {self.outCount}, outLastTime {self.outLastTime}, totalReward {self.totalReward}, lastClosestWayPoint {self.lastClosestWayPoint}')
+            return True
+        if (progress>99.5):
+            print(f'### marcel99Progression - iteration {self.iteration}, uuid {self.uuid}, progress {progress}, is_offtrack {is_offtrack}, stepCount {self.stepCount}, lastCurrentProgress {self.lastCurrentProgress}, lastProgress {self.lastProgress}, maxLastProgress {self.maxLastProgress}, totalProgress {self.totalProgress}, consumedEndProgressionBonus {self.consumedEndProgressionBonus}, outCount {self.outCount}, outLastTime {self.outLastTime}, totalReward {self.totalReward}, lastClosestWayPoint {self.lastClosestWayPoint}')
+        return False
 
+    def endLapV4(self,params):
+        newClosestWayPoint = params['closest_waypoints'][0]
+        if ((newClosestWayPoint+30)<self.lastClosestWayPoint):
+            self.lastClosestWayPoint=newClosestWayPoint
+            return True
+        else:
+            self.lastClosestWayPoint=newClosestWayPoint
+            return False
 
-reward_state = Reward()
+class Reward:
+    def __init__(self):
+        self.last_progress = 0
+        self.nb_sortie = 0
+        self.uuid = uuid.uuid4()
+        self.totalProgress = 0
+        self.totalReward = 0
+        self.jeromeStep = 0
+        self.lastClosest = 0
+
+    def reward_function(self, params):
+        self.jeromeStep += 1
+        all_wheels_on_track = params['all_wheels_on_track']
+        if not all_wheels_on_track:
+            self.nb_sortie += 1
+        #COucou
+        newLastClosest = params['closest_waypoints'][1]
+        speed = params['speed']
+        closest_waypoints = params['closest_waypoints']
+        nb_progress = params['progress'] - self.last_progress
+        if (nb_progress>0):
+            self.totalProgress += nb_progress
+        self.last_progress = params['progress']
+        currentStep = params['steps']
+        top = self.jeromeStep + (PUNITION_SORTIE_FACTOR * 45 * self.nb_sortie)
+        if top < TOP_CONST:
+            if not all_wheels_on_track:
+                reward = (100 - (top / 2)) * nb_progress
+            else :
+                reward = 1e-3
+        else:
+            reward = 1e-3
+        self.totalReward += reward
+        print(f'### jerome - uuid {self.uuid}, jeromeStep {self.jeromeStep}, currentStep {currentStep}, top {top}, closest_waypoints {closest_waypoints}, speed {speed}, nb_progress {nb_progress}, lastProgress {self.last_progress}, totalProgress {self.totalProgress}, nbSortie {self.nb_sortie}, all_wheels_on_track {all_wheels_on_track}, reward {reward} and totalReward {self.totalReward}')
+        return reward
+
+reward_state = RewardV3()
 
 
 def reward_function(params):
     print('params =>', params)
     reward = reward_state.reward_function(params)
     print('reward final result: ', reward)
-    return reward
+    return float(min(1e3, max(reward, 1e-3)))
