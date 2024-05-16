0df019bed18434c7bbe41f45a678e3db0c63b1c1
Ahmed REFAI-ext <ahmed.refai-ext@sanofi.com>
2024-05-16 20:24:39 +0200

    v5

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -1,42 +1,133 @@
 import math
 
-MAX_SPEED = 3.5
+MAX_SPEED = 4
 MIN_SPEED = 1
 MAX_STEERING = 25
 MIN_STEERING = -25
-FORCAST = 10
+MAX_VISION = 45
+MIN_VISION = -45
+FORCAST = 15
 STEPS_PER_SECOND = 15
 VEHICLE_WIDTH = 0.225
 
 
-def forcast(start_forcast_index, next_waypoint, waypoints, track_width, pos):
-    forcast_index = next_waypoint
-    if start_forcast_index > next_waypoint:
-        waypoints_middle = [i for i in range(start_forcast_index, next_waypoint - 1, -1)]
-    else:
-        waypoints_middle = [i for i in range(start_forcast_index, -1, -1)] + [i for i in range(len(waypoints) - 1, next_waypoint - 1, -1)]
-    for i in range(len(waypoints_middle)):
-        out_of_track = False
-        for j in range(i + 1, len(waypoints_middle)):
-            d = dist(waypoints[waypoints_middle[i]], pos, waypoints[waypoints_middle[j]])
-            if d >= 0.5 * track_width:
-                out_of_track = True
-                break
-        if not out_of_track:
-            forcast_index = waypoints_middle[i]
-            break
-    # print('projected waypoint:', forcast_index)
-    diff_index = forcast_index - next_waypoint if forcast_index > next_waypoint else forcast_index + len(waypoints) - 1 - next_waypoint
-    # print('diff_index:', diff_index)
-    return forcast_index, diff_index
+# def direction_line(pos, speed, heading):
+#     x, y = pos
+#     x1, y1 = x + (speed * math.cos(math.radians(heading))), y + (speed * math.sin(math.radians(heading)))
+#     a = (y1 - y) / (x1 - x)
+#     c = y - a * x
+#     return a, -1, c
+#
+#
+# def dist_from_line(pos, line):
+#     x1, y1 = pos
+#     a, b, c = line
+#     return abs(a * x1 + b * y1 + c) / math.sqrt(a ** 2 + b ** 2)
+
+# def forcast_v2(next_waypoint_index, waypoints, max_dist, pos, speed, heading):
+#     line = direction_line(pos, speed / STEPS_PER_SECOND, heading)
+#     forcast_index = next_waypoint_index
+#     depth = 0
+#     while dist_from_line(waypoints[forcast_index], line) < max_dist:
+#         depth += 1
+#         forcast_index = (forcast_index + 1) % len(waypoints)
+#     return (forcast_index - 1) % len(waypoints), depth
+
+# def forcast(start_forcast_index, next_waypoint, waypoints, track_width, pos):
+#     forcast_index = next_waypoint
+#     if start_forcast_index > next_waypoint:
+#         waypoints_middle = [i for i in range(start_forcast_index, next_waypoint - 1, -1)]
+#     else:
+#         waypoints_middle = [i for i in range(start_forcast_index, -1, -1)] + [i for i in range(len(waypoints) - 1, next_waypoint - 1, -1)]
+#     for i in range(len(waypoints_middle)):
+#         out_of_track = False
+#         for j in range(i + 1, len(waypoints_middle)):
+#             d = dist(waypoints[waypoints_middle[i]], pos, waypoints[waypoints_middle[j]])
+#             if d >= 0.5 * track_width:
+#                 out_of_track = True
+#                 break
+#         if not out_of_track:
+#             forcast_index = waypoints_middle[i]
+#             break
+#     # print('projected waypoint:', forcast_index)
+#     diff_index = forcast_index - next_waypoint if forcast_index > next_waypoint else forcast_index + len(waypoints) - 1 - next_waypoint
+#     # print('diff_index:', diff_index)
+#     return forcast_index, diff_index
 
 
 def dist(p1, p2, p3):
     return abs((p2[0] - p1[0]) * (p3[1] - p1[1]) - (p3[0] - p1[0]) * (p2[1] - p1[1])) / math.sqrt((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2)
 
 
+def direction_waypoint(pos, waypoint):
+    x, y = pos
+    waypoint_x, waypoint_y = waypoint
+    waypoint_direction = math.atan2(waypoint_y - y, waypoint_x - x)
+    res = math.degrees(waypoint_direction)
+    return res if res >= 0 else res + 360
+
+
+# def forcast_v3(next_waypoint_index, waypoints, pos, heading, max_dist):
+#     tmp_heading = heading if heading >= 0 else heading + 360
+#     r_line_direction, l_line_direction = tmp_heading + MIN_VISION, tmp_heading + MAX_VISION
+#     start_forcast_index = (next_waypoint_index + FORCAST - 1) % len(waypoints)
+#     if start_forcast_index > next_waypoint_index:
+#         middle_waypoints = [i for i in range(start_forcast_index, next_waypoint_index - 1, -1)]
+#     else:
+#         middle_waypoints = [i for i in range(start_forcast_index, -1, -1)] + [i for i in range(len(waypoints) - 1, next_waypoint_index - 1, -1)]
+#     diff_index = len(middle_waypoints)
+#     for i in range(len(middle_waypoints)):
+#         # check within cone
+#         waypoint_direction = direction_waypoint(pos, waypoints[middle_waypoints[i]])
+#         if r_line_direction <= waypoint_direction <= l_line_direction:
+#             visible = True
+#             for j in range(i + 1, len(middle_waypoints)):
+#                 d = dist(waypoints[middle_waypoints[i]], pos, waypoints[middle_waypoints[j]])
+#                 if d >= max_dist:
+#                     visible = False
+#                     break
+#             if visible:
+#                 diff_index = len(middle_waypoints) - i
+#                 break
+#             else:
+#                 diff_index -= 1
+#         else:
+#             diff_index -= 1
+#     return True, diff_index
+
+def forcast_v4(next_waypoint_index, waypoints, pos, heading, max_dist):
+    tmp_heading = heading if heading >= 0 else heading + 360
+    r_line_direction, l_line_direction = tmp_heading + MIN_VISION, tmp_heading + MAX_VISION
+    start_forcast_index = (next_waypoint_index + FORCAST - 1) % len(waypoints)
+    if start_forcast_index > next_waypoint_index:
+        middle_waypoints = [i for i in range(start_forcast_index, next_waypoint_index - 1, -1)]
+    else:
+        middle_waypoints = [i for i in range(start_forcast_index, -1, -1)] + [i for i in range(len(waypoints) - 1, next_waypoint_index - 1, -1)]
+    diff_index = len(middle_waypoints)
+    for i in range(len(middle_waypoints)):
+        # check within cone
+        waypoint_direction = direction_waypoint(pos, waypoints[middle_waypoints[i]])
+        if r_line_direction <= waypoint_direction <= l_line_direction:
+            visible = True
+            for j in range(i + 1, len(middle_waypoints)):
+                d = dist(waypoints[middle_waypoints[i]], pos, waypoints[middle_waypoints[j]])
+                if d >= max_dist:
+                    visible = False
+                    break
+            if visible:
+                diff_index = len(middle_waypoints) - i
+                break
+            else:
+                diff_index -= 1
+        else:
+            diff_index -= 1
+    return (next_waypoint_index + diff_index - 1) % len(waypoints), diff_index
+
+
 class Reward:
     prev_progress = 0
+    prev_steering = 0
+    prev_on_track = True
     total_reward = 0
 
     def __init__(self):
@@ -44,106 +135,85 @@ class Reward:
 
     def reset(self):
         self.prev_progress = 0
+        self.prev_steering = 0
+        self.prev_on_track = True
         self.total_reward = 0
 
+    def shortcut(self, speed, steering, progress, diff_direction):
+        speed_ratio = (speed - MIN_SPEED) / (MAX_SPEED - MIN_SPEED)
+        direction_ratio = diff_direction / MAX_VISION
+        # steering_ratio = abs(steering - self.prev_steering) / (2 * MAX_STEERING)
+        # progress_diff = progress - self.prev_progress
+        res = round((1 - speed_ratio) * (1 - direction_ratio), 3)
+        return res
+
+    def speedup(self, speed, steering, progress, diff_direction):
+        speed_ratio = (speed - MIN_SPEED) / (MAX_SPEED - MIN_SPEED)
+        direction_ratio = diff_direction / MAX_VISION
+        # steering_ratio = abs(steering - self.prev_steering) / (2 * MAX_STEERING)
+        # progress_diff = progress - self.prev_progress
+        res = round(speed_ratio * (1 - direction_ratio), 3)
+        return res
+
     def reward_function(self, params):
-        """
-        # waypoints = params['waypoints']
-        # closest_waypoints = params['closest_waypoints']
-        # track_width = params['track_width']
-        # potential_forcast_index = (closest_waypoints[1] + FORCAST) % len(waypoints)
-        # car_pos = (params['x'], params['y'])
-        # print('============')
-        # print('car forcast')
-        # forcast_car_index, diff_index = forcast(potential_forcast_index, closest_waypoints[1], waypoints, track_width, car_pos)
-        # forcast_car = waypoints[potential_forcast_index]
-
-        # print('============')
-        # print('track forcast')
-        # forcast_track_index = forcast(potential_forcast_index, closest_waypoints[1], waypoints, track_width, waypoints[closest_waypoints[0]])
-        # forcast_track = waypoints[forcast_track_index]
-
-        # steering
-        # if self.prev_steering_angle is not None:
-        #     prev_steering_angle = self.prev_steering_angle
-        # else:
-        #     prev_steering_angle = params['steering_angle']
-        # self.prev_steering_angle = params['steering_angle']
-        # diff_steering = abs(prev_steering_angle - params['steering_angle'])
-        # print('diff_steering in degrees:', diff_steering)
-        # prev_steering_angle = self.prev_steering_angle if self.prev_steering_angle is not None else 0
-        # steering_angle = params['steering_angle']
-        # steering_diff = abs(steering_angle - prev_steering_angle)
-        # print('steering_diff in degrees:', steering_diff)
-        # if steering_diff > 30:
-        #     return 1e-3
-        # reward_steering = 10 * (1 - (steering_diff / 30))
-        # print('reward_steering:', reward_steering)
-        # self.prev_steering_angle = steering_angle
-
-        # direction
-        # car_direction = math.atan2(forcast_car[1] - car_pos[1], forcast_car[0] - car_pos[0])
-        # car_direction = math.degrees(car_direction)
-        # heading = params['heading']
-        # direction_diff = abs(car_direction - heading)
-        # if direction_diff > 180:
-        #     direction_diff = 360 - direction_diff
-        # print('direction_diff:', direction_diff)
-        # print('============')
-
-        # speed
-        # p_w = waypoints[closest_waypoints[0]]
-        # n_w = waypoints[closest_waypoints[1]]
-        # curve_forcast = waypoints[(closest_waypoints[1] + 3) % len(waypoints)]
-        # track_curve = math.atan2(n_w[1] - p_w[1], n_w[0] - p_w[0]) - math.atan2(curve_forcast[1] - p_w[1], curve_forcast[0] - p_w[0])
-        # track_curve = abs(math.degrees(track_curve))
-        # if track_curve > 180:
-        #     track_curve = 360 - track_curve
-        # print('track_curve:', track_curve)
-        # print('track_curve:', track_curve)
-        # if track_curve < 45:
-        #     return params['speed']
-        # elif 45 <= track_curve < 60:
-        #     return params['speed'] * 0.5
-        # else:
-        #     return 1e-3
-        # if direction_diff <= 90:
-        # else:
-        #     target_speed = -4
-        # target_speed = (MAX_SPEED - MIN_SPEED) * math.exp(-5 * (track_curve / 90) ** 2) + MIN_SPEED
-        # target_speed = MAX_SPEED - ((MAX_SPEED - MIN_SPEED) / 90) * track_curve
-        # print('target_speed:', target_speed)
-        # speed_diff = abs(target_speed - params['speed'])
-        # print('speed_diff:', speed_diff)
-        # print('============')
-        # reward = -(2 * x) - (2 * y) + 1
-        # reward = - 5 * x ** 2 - 5 * y ** 2 + 1.001
-        # reward = (MAX_SPEED - MIN_SPEED) - ((MAX_SPEED - MIN_SPEED) / 90) * track_curve
-
-        # progress_diff = params['progress'] - self.progress
-        # self.progress = params['progress']
-        # reward = (progress_diff / self.steps)
-        """
         reward = 1e-3
         all_wheels_on_track = params['all_wheels_on_track']
         track_width = params['track_width']
         distance_from_center = params['distance_from_center']
         progress = params['progress']
         speed = params['speed']
+        steering = params['steering_angle']
         waypoints = params['waypoints']
         closest_waypoints = params['closest_waypoints']
-        MAX_DIST = 0.5 * (track_width + VEHICLE_WIDTH)
-        if all_wheels_on_track and distance_from_center < MAX_DIST:
-            potential_forcast_index = (closest_waypoints[1] + FORCAST) % len(waypoints)
-            car_pos = (params['x'], params['y'])
-            forcast_car_index, diff_index = forcast(potential_forcast_index, closest_waypoints[1], waypoints, track_width, car_pos)
-            # w1 = round(1 - (diff_index / FORCAST) ** 3, 2)
-            w1 = math.exp(-50*(diff_index / FORCAST) ** 15)
-            w2 = 1 - w1
-            reward = round(100 * w1, 2) * (progress - self.prev_progress) + round(100 * w2, 2) * (speed / MAX_SPEED)
-            print(f'{closest_waypoints}, {diff_index}')
-            print(f'{reward} = {round(100 * w1, 2)} * {progress - self.prev_progress} + {round(100 * w2, 2)} * {speed / MAX_SPEED}')
-        self.prev_progress = progress
+        heading = params['heading']
+        car_coord = (params['x'], params['y'])
+        # Calculate the direction of the center line based on the closest waypoints
+        next_point = waypoints[closest_waypoints[1]]
+        prev_point = waypoints[closest_waypoints[0]]
+        track_direction = math.atan2(next_point[1] - prev_point[1], next_point[0] - prev_point[0])
+        # Convert to degree
+        track_direction = math.degrees(track_direction)
+        # true_heading = heading + steering
+        # if true_heading > 180:
+        #     true_heading = 360 - true_heading
+        # elif true_heading < -180:
+        #     true_heading = 360 + true_heading
+        true_heading = heading
+        # Calculate the difference between the track direction and the heading direction of the car
+        direction_diff = abs(track_direction - true_heading)
+        if direction_diff > 180:
+            direction_diff = 360 - direction_diff
+        max_dist = 0.5 * (track_width + VEHICLE_WIDTH)
+        if self.prev_on_track and distance_from_center < max_dist and direction_diff < 90:
+            # forcast_car_index, diff_index = forcast_v2(closest_waypoints[1], waypoints, MAX_DIST, car_coord, speed, true_heading)
+            forcast_index, diff_index = forcast_v4(closest_waypoints[1], waypoints, car_coord, true_heading, max_dist)
+            # potential_forcast_index = (closest_waypoints[1] + FORCAST) % len(waypoints)
+            # forcast_car_index, diff_index = forcast(potential_forcast_index, closest_waypoints[1], waypoints, MAX_DIST, car_coord)
+            # print(forcast_car_index, diff_index)
+            # forcast_car_coord = waypoints[forcast_car_index]
+            # r = diff_index / FORCAST
+            w1 = round(1 / (1 + math.exp(diff_index - 8)), 3)
+            w2 = round(1 - w1, 3)
+            forcast_car_coord = waypoints[forcast_index]
+            forcast_direction = math.atan2(forcast_car_coord[1] - car_coord[1], forcast_car_coord[0] - car_coord[0])
+            forcast_direction = math.degrees(forcast_direction)
+            forcast_direction_diff = abs(forcast_direction - heading)
+            if forcast_direction_diff > 180:
+                forcast_direction_diff = 360 - forcast_direction_diff
+            print(f'variables: {closest_waypoints}, '
+                  f'{diff_index}, '
+                  f'{forcast_index}, '
+                  f'{(speed - MIN_SPEED) / (MAX_SPEED - MIN_SPEED)}, '
+                  # f'{0.2 * (1 - (abs(steering - self.prev_steering) / (2 * MAX_STEERING)))}, '
+                  # f'{progress - self.prev_progress}, '
+                  f'{1 - (forcast_direction_diff / MAX_VISION)}')
+            x, y = self.shortcut(speed, steering, progress, forcast_direction_diff), self.speedup(speed, steering, progress, forcast_direction_diff)
+            reward = round((w1 * x + w2 * y), 3)
+            print(f'reward {reward} = ({w1} * {x} + {w2} * {y})')
+            self.prev_progress = progress
+            self.prev_steering = steering
+        else:
+            self.prev_on_track = False
         self.total_reward += reward
         is_complete_lap = int(progress) == 100
         is_offtrack = params['is_offtrack']
@@ -151,9 +221,6 @@ class Reward:
         is_crashed = params['is_crashed']
         is_final_step = is_complete_lap or is_offtrack or is_reversed or is_crashed
         if is_final_step:
-            # if is_complete_lap:
-            #     reward += 0.1 * self.total_reward
-            #     print(f'completed lap with original total reward {self.total_reward} and new reward {reward}')
             self.reset()
         return float(min(1e3, max(reward, 1e-3)))
 
