6a4f44cb060a891470489f77c8f5db5e11eafc9a
Ahmed REFAI-ext <ahmed.refai-ext@sanofi.com>
2024-05-18 18:53:38 +0200

    v16

--- custom-files/reward_function.py
+++ custom-files/reward_function.py
@@ -144,7 +144,7 @@ class Reward:
         # steering_ratio = abs(steering - self.prev_steering) / (2 * MAX_STEERING)
         # steering_ratio_1 = abs(steering) / MAX_STEERING
         progress_diff = progress - self.prev_progress
-        res = round((1 - speed_ratio) * progress_diff, 3)
+        res = round(progress_diff + 0.5 * (1 - speed_ratio), 3)
         return res
 
     def speedup(self, speed, steering, progress, diff_direction):
@@ -152,7 +152,7 @@ class Reward:
         direction_ratio = diff_direction / MAX_VISION
         # steering_ratio = abs(steering - self.prev_steering) / (2 * MAX_STEERING)
         # progress_diff = progress - self.prev_progress
-        res = round(speed_ratio * (1 - direction_ratio), 3)
+        res = round(speed_ratio + 0.5 * (1 - direction_ratio), 3)
         return res
 
     def reward_function(self, params):
